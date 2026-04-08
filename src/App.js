cee15d4271faa7723a26baec77686552445cfea2
ahmadrazi0077-maker <ahmadrazi0077@gmail.com>
2026-04-08 13:11:59 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
-import { ThemeProvider } from './context/ThemeContext';
-import { SocketProvider } from './context/SocketContext';
-import AppRoutes from './routes/AppRoutes';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/layout/Layout';
+import Home from './pages/Home';
+import Teams from './pages/Teams';
+import TeamDetail from './pages/TeamDetail';
+import Players from './pages/Players';
+import PlayerDetail from './pages/PlayerDetail';
+import Matches from './pages/Matches';
+import LiveMatch from './pages/LiveMatch';
+import Standings from './pages/Standings';
+import News from './pages/News';
+import NewsDetail from './pages/NewsDetail';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <BrowserRouter>
-      <ThemeProvider>
-        <AuthProvider>
-          <SocketProvider>
-            <AppRoutes />
-          </SocketProvider>
-        </AuthProvider>
-      </ThemeProvider>
-    </BrowserRouter>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/teams" element={<Teams />} />
+          <Route path="/teams/:id" element={<TeamDetail />} />
+          <Route path="/players" element={<Players />} />
+          <Route path="/players/:id" element={<PlayerDetail />} />
+          <Route path="/matches" element={<Matches />} />
+          <Route path="/matches/:id" element={<LiveMatch />} />
+          <Route path="/standings" element={<Standings />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/news/:id" element={<NewsDetail />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
 
