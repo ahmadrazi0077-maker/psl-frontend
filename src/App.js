772376d8c6f1dd32f444c0a63d5764dc9ba57671
ahmadrazi0077-maker <ahmadrazi0077@gmail.com>
2026-04-14 22:48:38 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import Teams from './pages/Teams';
-import TeamDetail from './pages/TeamDetail';
-import Players from './pages/Players';
-import PlayerDetail from './pages/PlayerDetail';
-import Matches from './pages/Matches';
-import Standings from './pages/Standings';
-import News from './pages/News';
-import NewsDetail from './pages/NewsDetail';
+import { BrowserRouter } from 'react-router-dom';
+import AppRoutes from './routes/AppRoutes';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/teams" element={<Teams />} />
-        <Route path="/teams/:id" element={<TeamDetail />} />
-        <Route path="/players" element={<Players />} />
-        <Route path="/players/:id" element={<PlayerDetail />} />
-        <Route path="/matches" element={<Matches />} />
-        <Route path="/standings" element={<Standings />} />
-        <Route path="/news" element={<News />} />
-        <Route path="/news/:id" element={<NewsDetail />} />
-      </Routes>
-    </Router>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
 
