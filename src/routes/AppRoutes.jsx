ed6c159eceec94a6567838edbbd545b03299e6cf
ahmadrazi0077-maker <ahmadrazi0077@gmail.com>
2026-04-17 22:33:55 +0500

    Update AppRoutes.jsx

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,6 +2,7 @@ import React, { Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Layout from '../components/layout/Layout';
 import Loader from '../components/common/Loader';
+import LiveScores from './pages/LiveScores';
 
 // Lazy load pages
 const Home = lazy(() => import('../pages/Home'));
@@ -29,6 +30,7 @@ const AppRoutes = () => {
           <Route path="standings" element={<Standings />} />
           <Route path="news" element={<News />} />
           <Route path="news/:id" element={<NewsDetail />} />
+          <Route path="/live-scores" element={<LiveScores />} />
         </Route>
       </Routes>
     </Suspense>
