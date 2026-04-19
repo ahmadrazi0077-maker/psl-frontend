50b63c0c88f498c1c1685526fdbc808c2666f8de
ahmadrazi0077-maker <ahmadrazi0077@gmail.com>
2026-04-19 22:04:10 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './routes/AppRoutes';
-import { SpeedInsights } from "@vercel/speed-insights/react"
-import { Analytics } from "@vercel/analytics/react"
+
 
 function App() {
   return (
