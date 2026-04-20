d6b360fe73e4958a7f1cdd5fb5f0655320bf307c
ahmadrazi0077-maker <ahmadrazi0077@gmail.com>
2026-04-21 02:06:29 +0500

    Update TeamDetail.jsx

--- src/pages/TeamDetail.jsx
+++ src/pages/TeamDetail.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { supabase } from '../lib/supabase';
-
+import SEO from '../components/SEO';
 const TeamDetail = () => {
   const { id } = useParams();
   const [team, setTeam] = useState(null);
