21a26173bf3773fb54167662b46d905ece5ab102
ahmadrazi0077-maker <ahmadrazi0077@gmail.com>
2026-04-21 01:44:13 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  if (rootElement.hasChildNodes()) {
+    // Prerendered HTML exists - hydrate it
+    ReactDOM.hydrateRoot(rootElement, <App />);
+  } else {
+    // Normal SPA mode
+    ReactDOM.createRoot(rootElement).render(<App />);
+  }
+}
